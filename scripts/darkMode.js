94cc687ffa1147ae24fc670daece43969aaab62e
josef <josef.abdulkarim@hotmail.co.uk>
2025-07-02 17:33:01 +0100

    Added github workflow and fixed bug where javascript only worked on the index page

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -4,7 +4,12 @@ function darkMode() {
     document.body.classList.toggle("dark");
     document.querySelector('header').classList.toggle("dark");
     document.getElementById("navigationBar").classList.toggle("dark");
-    document.getElementsByClassName("profile").item(0).classList.toggle("dark");
+
+    var profile = document.getElementsByClassName("profile").item(0);
+    if (profile) {
+        profile.classList.toggle("dark");
+    }
+    // document.getElementsByClassName("profile").item(0).classList.toggle("dark");
     // document.getElementsByClassName("card").item(0).classList.toggle("dark");
 
     // console.log(document.getElementsByClassName("card").length);
