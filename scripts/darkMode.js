21dd2f6bc5491206d9b5d55e9dd19b1b564c1572
josef <josef.abdulkarim@hotmail.co.uk>
2025-07-04 12:13:40 +0100

    Reworked projects page among other improvements. Also added CV Download

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -4,11 +4,23 @@ function darkMode() {
     document.body.classList.toggle("dark");
     document.querySelector('header').classList.toggle("dark");
     document.getElementById("navigationBar").classList.toggle("dark");
+    document.getElementById("dark-mode-button").classList.toggle("dark");
 
     var profile = document.getElementsByClassName("profile").item(0);
     if (profile) {
         profile.classList.toggle("dark");
     }
+
+    var cards = document.getElementsByClassName("card");
+    for (var i = 0; i < cards.length; i++) {
+        document.getElementsByClassName("card")[i].classList.toggle("dark");
+    }
+
+    var downloadButton = document.getElementsByClassName("btn").item(0);
+    if (downloadButton) {
+        downloadButton.classList.toggle("dark");
+    }
+
     // document.getElementsByClassName("profile").item(0).classList.toggle("dark");
     // document.getElementsByClassName("card").item(0).classList.toggle("dark");
 
@@ -25,10 +37,14 @@ function darkMode() {
     //     button.classList.toggle("dark");
     // }
 
+    let darkModeImage = document.getElementById("dark-mode-image");
+
     if (document.body.classList.contains("dark")) {
         localStorage.setItem('theme', 'dark');
+        darkModeImage.src = "images/lightmode.png";
     } else {
         localStorage.removeItem('theme');
+        darkModeImage.src = "images/darkmode.png";
     }
 }
 
