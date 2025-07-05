5dac71b3bbf1ab14760113d05ba71e56373c2753
josef <josef.abdulkarim@hotmail.co.uk>
2025-07-05 19:25:08 +0100

    Fixed default theme, now correctly sets the theme to the browsers preference

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -2,7 +2,7 @@ let savedTheme = localStorage.getItem("theme");
 let systemPreference = window.matchMedia("(prefers-color-scheme: dark)");
 
 // Load theme button image into 'themeImage'
-if (savedTheme === "dark" || (!savedTheme && systemPreference)) {
+if (savedTheme === "dark" || (!savedTheme && systemPreference.matches)) {
     // Switch to dark theme
     document.documentElement.classList.add("dark");
 
