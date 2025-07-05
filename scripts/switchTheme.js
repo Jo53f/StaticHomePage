70f7a7e5736a22d59a5e4d4041076860a25e7a31
josef <josef.abdulkarim@hotmail.co.uk>
2025-07-05 18:44:56 +0100

    Fixed dark mode toggle showing wrong icon

--- scripts/switchTheme.js
+++ scripts/switchTheme.js
@@ -4,13 +4,17 @@ function changeTheme() {
     if (theme === "dark") {
         document.documentElement.classList.remove("dark");
         localStorage.setItem("theme", "light");
-        themeImage.src = "images/lightmode.png";
+        themeImage.src = "images/darkmode.png";
     } else {
         localStorage.setItem("theme", "dark");
         document.documentElement.classList.add("dark");
-        themeImage.src = "images/darkmode.png";
+        themeImage.src = "images/lightmode.png";
     }
 }
 
 let themeButton = document.getElementById("theme-mode-button");
 themeButton.addEventListener("click", changeTheme);
+
+if (localStorage.getItem("theme") === "dark") {
+    document.getElementById("theme-mode").src = "images/lightmode.png";
+}
