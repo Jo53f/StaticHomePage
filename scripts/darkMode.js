3b70abbaf4b924984849004f504d43450130fd6f
josef <josef.abdulkarim@hotmail.co.uk>
2025-07-05 17:53:17 +0100

    theme switch reimplemented, fixing bug where the screen would flicker when first loading or changing page.

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,58 +1,32 @@
-let savedTheme;
+let savedTheme = localStorage.getItem("theme");
+let systemPreference = window.matchMedia("(prefers-color-scheme: dark)");
 
-function darkMode() {
-    document.body.classList.toggle("dark");
-    document.querySelector('header').classList.toggle("dark");
-    document.getElementById("navigationBar").classList.toggle("dark");
-    document.getElementById("dark-mode-button").classList.toggle("dark");
+// Load theme button image into 'themeImage'
+let themeImage = document.getElementById("theme-mode");
 
-    var profile = document.getElementsByClassName("profile").item(0);
-    if (profile) {
-        profile.classList.toggle("dark");
-    }
+if (savedTheme === "dark" || (!savedTheme && systemPreference)) {
+    // Switch to dark theme
+    document.documentElement.classList.add("dark");
 
-    var cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-        document.getElementsByClassName("card")[i].classList.toggle("dark");
-    }
+    // Save default theme to local storage
+    localStorage.setItem("theme", "dark");
 
-    var downloadButton = document.getElementsByClassName("btn").item(0);
-    if (downloadButton) {
-        downloadButton.classList.toggle("dark");
-    }
+    // Change button Image
+    themeImage.src = "images/lightmode.png";
+}
 
-    // document.getElementsByClassName("profile").item(0).classList.toggle("dark");
-    // document.getElementsByClassName("card").item(0).classList.toggle("dark");
-
-    // console.log(document.getElementsByClassName("card").length);
-
-
-    // let cards = document.getElementsByClassName("card");
-    // for (let i = 0; i < cards.length; i++) {
-    //     cards[i].classList.toggle("dark");
-    // }
-
-    // const navButtons = document.getElementById("navigationBar").children[0].children;
-    // for (let button of navButtons) {
-    //     button.classList.toggle("dark");
-    // }
-
-    let darkModeImage = document.getElementById("dark-mode-image");
-
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem('theme', 'dark');
-        darkModeImage.src = "images/lightmode.png";
+function changeTheme() {
+    let theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.documentElement.classList.remove("dark");
+        localStorage.removeItem("theme");
+        themeImage.src = "images/lightmode.png";
     } else {
-        localStorage.removeItem('theme');
-        darkModeImage.src = "images/darkmode.png";
+        localStorage.setItem("theme", "dark");
+        document.documentElement.classList.add("dark");
+        themeImage.src = "images/darkmode.png";
     }
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        darkMode();
-    }
-})
-
-document.getElementById("dark-mode-button").addEventListener('click', darkMode);
+let themeButton = document.getElementById("theme-mode-button");
+themeButton.addEventListener("click", changeTheme);
